92f93558916902c70410d4f05ab94ef81c10891d
nidhin.pattaniyil <nidhin.pattaniyil@ableto.com>
2018-04-13 06:02:19 -0400

    added port as arg

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def similar():
     return jsonify(payload) , status_code  
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT",5000)
+    port = int(os.environ.get("PORT","5000"))
     app.run(host="0.0.0.0", port=port)    
